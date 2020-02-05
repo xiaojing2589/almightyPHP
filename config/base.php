50a86e198e46da456530155165e5de97f115b912
xiaojing2589 <125917130@qq.com>
2020-02-05 15:01:30 +0800

    常规提交

---
+++ config/base.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    // 产品信息
+    'product_name'    => 'xxxx',
+    'product_version' => '1.4.0',
+    'build_version'   => '201902242038',
+    'product_website' => 'http://www.xxxx.com',
+    'product_update'  => 'http://www.xxxx.com/checkUpdate',
+    'develop_team'    => 'xxxx',
+
+    // 公司信息
+    'company_name'    => 'xxxx科技公司',
+    'company_website' => 'http://www.xxxx.com',
+];
---
+++ config/console.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'name'      => 'Think Console',
+    'version'   => '0.1',
+    'user'      => null,
+    'auto_path' => env('app_path') . 'command' . DIRECTORY_SEPARATOR,
+];
