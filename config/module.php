7e02ea7e058665febf73b9a328104167a65262f8
xiaojing2589 <125917130@qq.com>
2020-02-06 21:19:23 +0800

    常规提交

--- config/module.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 模块相关设置
- */
-return [
-    // 使用默认控制器层的模块
-    'default_controller_layer' => ['admin', 'index', 'common', 'extra']
-];
--- config/system.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    // 拒绝ie访问
-    'deny_ie'       => false,
-    // 模块管理中，不读取模块信息的目录
-    'except_module' => ['common', 'admin', 'index', 'extra', 'user', 'install'],
-    'extend_cache'  => ['type' => 'file', 'expire' => 0, 'prefix' => 'extend_cache', 'path' => '../runtime/cache/']
-];
